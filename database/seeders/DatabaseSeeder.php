05808745cf7901f01d4626aa60fc0c62c9b13eda
Dimas Cahyo Nugroho <57878720+dimascahyo557@users.noreply.github.com>
2023-06-07 16:22:32 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::findOrCreate([
+            'email' => 'admin@gmail.com',
+        ], [
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'name' => 'Administrator',
+        ]);
     }
 }
